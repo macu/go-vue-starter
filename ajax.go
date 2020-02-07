7e68f4e83535cc1bb0d1dc7bb703a7f71d6adb65
Matt Cudmore <cudmore.mb@gmail.com>
2020-02-07 17:08:47 -0400

    Add AjaxRoute

--- ajax.go
+++ ajax.go
@@ -7,11 +7,15 @@ import (
 	"net/http"
 )
 
-var ajaxHandlers = map[string]map[string]AuthenticatedRoute{
-	http.MethodGet: map[string]AuthenticatedRoute{
+// AjaxRoute represents an authenticated AJAX handler that returns
+// a response object to be sent as JSON, or an error to log, and a status code.
+type AjaxRoute func(db *sql.DB, userID uint, w http.ResponseWriter, r *http.Request) (interface{}, int, error)
+
+var ajaxHandlers = map[string]map[string]AjaxRoute{
+	http.MethodGet: map[string]AjaxRoute{
 		"/ajax/test": ajaxTest,
 	},
-	http.MethodPost: map[string]AuthenticatedRoute{},
+	http.MethodPost: map[string]AjaxRoute{},
 }
 
 func ajaxHandler(db *sql.DB, userID uint, w http.ResponseWriter, r *http.Request) {
@@ -19,25 +23,33 @@ func ajaxHandler(db *sql.DB, userID uint, w http.ResponseWriter, r *http.Request
 	if foundMethod {
 		handler, fouundPath := handlers[r.URL.Path]
 		if fouundPath {
-			handler(db, userID, w, r)
+			response, statusCode, err := handler(db, userID, w, r)
+			if err != nil {
+				log.Printf("Error running ajax handler [%s]: %v\n", r.URL.Path, err)
+				w.WriteHeader(statusCode)
+				return
+			}
+			if response != nil {
+				js, err := json.Marshal(response)
+				if err != nil {
+					log.Printf("Error marshalling response: %v\n", err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(statusCode)
+				w.Write(js)
+			}
 			return
 		}
 	}
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func ajaxTest(db *sql.DB, userID uint, w http.ResponseWriter, r *http.Request) {
+func ajaxTest(db *sql.DB, userID uint, w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	response := struct {
 		Message string `json:"message"`
 	}{"Message retrieved using AJAX"}
 
-	js, err := json.Marshal(response)
-	if err != nil {
-		log.Printf("Error marshalling response: %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	return response, http.StatusOK, nil
 }
